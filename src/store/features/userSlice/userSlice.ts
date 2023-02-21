350c9d7fd924d54acb9c2888cbc2e3f3c1596af3
Marc Girbau <marc.girbau94@gmail.com>
2023-02-21 19:50:58 +0100

    Create userSlice with loginUser and logoutUser cases

---
+++ src/store/features/userSlice/userSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User, UserState } from "./types";
+
+const initialState: UserState = {
+  username: "",
+  isLogged: false,
+  token: "",
+  id: "",
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginUser: (currentUserState, action: PayloadAction<User>): UserState => ({
+      ...action.payload,
+      isLogged: true,
+    }),
+
+    logoutUser: (): UserState => ({ ...initialState }),
+  },
+});
+
+export const {
+  loginUser: loginUserActionCreator,
+  logoutUser: logoutUserActionCreator,
+} = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
