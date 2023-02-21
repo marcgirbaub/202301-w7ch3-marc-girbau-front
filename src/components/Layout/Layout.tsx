21d38e9c83b520f1c873d842e3e417127ec2f944
Marc Girbau <marc.girbau94@gmail.com>
2023-02-22 00:37:52 +0100

    Add styles to CreatePage

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,16 +6,31 @@ const Layout = (): JSX.Element => {
     <LayoutStyled>
       <header className="main-header">
         <h1 className="main-header__title">Robots</h1>
-        <Link to={"/"}>
-          <button type="button" className="btn btn-secondary">
-            Home
-          </button>
-        </Link>
-        <Link to={"login"}>
-          <button type="button" className="btn btn-primary">
-            Login
-          </button>
-        </Link>
+        <nav>
+          <ul className="main-header__nav nav">
+            <li>
+              <Link to={"/"}>
+                <button type="button" className="btn btn-secondary">
+                  Home
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to={"create"}>
+                <button type="button" className="btn btn-success">
+                  Create
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to={"login"}>
+                <button type="button" className="btn btn-primary">
+                  Login
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
       <Outlet />
     </LayoutStyled>
--- src/pages/CreatePage/CreatePage.tsx
+++ src/pages/CreatePage/CreatePage.tsx
@@ -1,10 +1,11 @@
 import Form from "../../components/Form/Form";
+import CreatePageStyled from "./CreatePageStyles";
 
 const CreatePage = (): JSX.Element => {
   return (
-    <>
+    <CreatePageStyled>
       <Form />
-    </>
+    </CreatePageStyled>
   );
 };
 
